7bfd0fe050314fab4fa52d3226cede1f44d2d24b
kalator <michael.kala@email.cz>
2018-11-01 11:52:20 +0100

    Initial commit.

---
+++ src/server.R
@@ -0,0 +1,37 @@
+#install.packages("geoR")
+
+
+setwd("/home/michael/skola/sgs/shiny_app/")
+
+library(geoR)
+data(s100)
+
+cloud1 <- variog(s100, option = "cloud", max.dist=1)
+cloud2 <- variog(s100, option = "cloud", estimator.type = "modulus", max.dist=1)
+bin1 <- variog(s100, uvec=seq(0,1,l=11))
+bin2  <- variog(s100, uvec=seq(0,1,l=11), estimator.type= "modulus")
+
+
+
+
+shinyServer
+(
+  function(input,output,session)
+    {
+      output$variogram <- renderPlot({
+        choosen <-switch(input$vario_type,
+                         'Spherical' = "sph",
+                         'Exponential' = "exp",
+                         'Gaussian' = "gaussian",
+                         'Cubic' = 'cubic'
+                       )
+        
+        plot(bin1)
+        lines.variomodel(cov.model = choosen, cov.pars = c(1,0.3), nugget = 0, max.dist = 1,  lwd = 3)
+      
+      }      )  
+    
+    
+  }
+ 
+) 
---
+++ src/ui.R
@@ -0,0 +1,88 @@
+#install.packages("shiny")
+# install.packages("shinydashboard")
+
+library(shiny)
+library(shinydashboard)
+
+shinyUI(
+  dashboardPage(
+    dashboardHeader(title = "Kriging", 
+                    titleWidth = 200
+                    ),
+    
+    dashboardSidebar(width = 200,
+      sidebarMenu(id = 'sidebarmenu',
+                  menuItem('Introduction', 
+                           tabName = "Introduction"
+                          ),
+                  
+                  menuItem('Geostatistics',
+                           tabName = 'geostatistics'
+                           ),
+                  
+                  menuItem('Kriging',
+                           tabName = 'kriging',
+                           menuSubItem('Kriging in general',
+                                       tabName = 'kriging_in_general'
+                                       ),
+                           
+                           menuSubItem('Non robust kriging',
+                                       tabName = 'kriging_non_robust'
+                                       ),
+                           
+                           menuSubItem('Robust kriging',
+                                       tabName = 'kriging_robust'
+                                       )
+                           )
+                  
+                  )
+                    ),
+    dashboardBody(
+      tabItems(
+        tabItem("Introduction", 
+                h2("Introduction"),
+                hr(),
+                includeText("introduction.txt")
+                ),
+        
+        tabItem("geostatistics", 
+                h2("Geostatistics"),
+                hr(),
+                includeText("geostatistics.txt")
+                ),
+
+        tabItem("kriging_in_general",
+                h2("Kriging in general"),
+                hr(),
+                includeText("general_kriging.txt")
+                ),
+        
+        tabItem("kriging_non_robust", 
+                h2("Non robust kriging"),
+                "blahblahblah",
+                fluidRow(
+                box(width = 4, 
+                    selectInput('vario_type', 
+                                label = 'Choose theoretical variogram:', 
+                                choices = c('Spherical',
+                                            'Exponential',
+                                            'Gaussian',
+                                            'Cubic')
+                                          )
+                   ),
+                box(width = 7, 
+                    plotOutput("variogram")
+                   )
+                        )
+                ),
+        
+        tabItem("kriging_robust", 
+                h2("Robust kriging")
+                )
+        
+               )
+      
+                   )
+           )
+  
+      )
